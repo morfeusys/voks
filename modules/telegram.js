38726dff12400952b010799aa7fee3f3df539a2a
morfeusys <che@just-ai.com>
2022-12-07 19:08:47 +0300

    reply to voice message

--- modules/telegram.js
+++ modules/telegram.js
@@ -11,14 +11,17 @@ module.exports = token => {
         bot.sendMessage(msg.chat.id, "Send me some voice note and I'll decode it to the text.");
     });
 
-    bot.on("voice", msg => {
-        let file_id = msg.voice.file_id
-        let chat_id = msg.chat.id
+    bot.on("voice", voiceMessage => {
+        let file_id = voiceMessage.voice.file_id
+        let chat_id = voiceMessage.chat.id
         bot.sendMessage(chat_id, "*_Decoding\\.\\.\\. Please wait a bit_* 💬", {parse_mode: "MarkdownV2"}).then(msg => {
             decoder.decode(bot.getFileStream(file_id)).then(result => {
                 if (result) {
                     console.log(result)
-                    bot.editMessageText(result, {chat_id: chat_id, message_id: msg.message_id})
+                    bot.deleteMessage(chat_id, msg.message_id)
+                    bot.sendMessage(chat_id, result, {
+                        reply_to_message_id: voiceMessage.message_id
+                    })
                 } else {
                     console.log("decoding error")
                     bot.editMessageText("I cannot decode that, sorry...", {chat_id: chat_id, message_id: msg.message_id})
